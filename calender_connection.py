a2e6eadc22f9ade18f1157f2a61cdd68a9f71421
EADALARAMYA31 <ramyaeadala@gmail.com>
2026-06-06 17:32:25 +0530

    finalup8

--- calender_connection.py
+++ calender_connection.py
@@ -55,8 +55,6 @@ def handle_oauth_callback(code):
         scopes=SCOPES,
         redirect_uri=REDIRECT_URI
     )
-    # 🔥 IMPORTANT: disable PKCE issues
-    flow.redirect_uri = REDIRECT_URI
 
     flow.fetch_token(code=code)
 
