d6155c0db06f3e5ac852ea09145b67e693b50d66
EADALARAMYA31 <ramyaeadala@gmail.com>
2026-06-06 20:29:28 +0530

    finalup37

--- calender_connection.py
+++ calender_connection.py
@@ -55,32 +55,26 @@ def get_calendar_auth_url():
 # =====================
 def handle_oauth_callback(code):
     try:
+        with open("oauth_verifier.txt", "r") as f:
+            verifier = f.read()
+
         flow = Flow.from_client_config(
             client_config,
             scopes=SCOPES,
             redirect_uri=REDIRECT_URI
         )
 
-        flow.code_verifier = st.session_state.get("code_verifier")
-
-        st.write("STEP 1: Before fetch_token")
+        flow.code_verifier = verifier
 
         flow.fetch_token(code=code)
-
-        st.write("STEP 2: fetch_token SUCCESS")
-
-        creds = flow.credentials
-
-        with open("token.pkl", "wb") as token:
-            pickle.dump(creds, token)
-
-        st.write("STEP 3: token.pkl SAVED")
-
-        return creds
+        st.success("TOKEN SUCCESS")
+        return flow.credentials
 
     except Exception as e:
-        st.error(f"ERROR = {e}")
-        return None
+        st.error(repr(e))
+        import traceback
+        st.code(traceback.format_exc())
+    return None
 # =========================
 # LOAD CREDENTIALS
 # =========================
