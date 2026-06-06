f7835e2f5d3e039f91f3486289c9156671fe775a
EADALARAMYA31 <ramyaeadala@gmail.com>
2026-06-06 16:40:13 +0530

    finalup1

--- calender_connection.py
+++ calender_connection.py
@@ -35,7 +35,7 @@ def get_calendar_auth_url():
 
     auth_url, state = flow.authorization_url(
         access_type="offline",
-        include_granted_scopes=True,
+        include_granted_scopes="true",
         prompt="consent"
     )
 
